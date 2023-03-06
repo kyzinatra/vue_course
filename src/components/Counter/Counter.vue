8b6b303b9fa044d666df46337a42b1b616559097
kyzinatra <main@kyzinatra.ru>
2023-03-06 13:55:35 +0300

    lesson 1. Introduction

---
+++ src/components/Counter/Counter.vue
@@ -0,0 +1,27 @@
+<script setup>
+  import { reactive, ref, watchEffect, watch } from "vue";
+
+  const counter = ref(0);
+  const date = ref(new Date().toLocaleTimeString());
+  function counterHandler(event) {
+    console.log();
+    counter.value++;
+  }
+
+  watch(
+    {},
+    () => {
+      setInterval(() => {
+        date.value = new Date().toLocaleTimeString();
+      }, 1000);
+    },
+    { immediate: true }
+  );
+</script>
+
+<template>
+  <button @click="counterHandler">counter {{ counter }}</button>
+  <h1>{{ date }}</h1>
+</template>
+
+<style scoped></style>
